db24d74b2a7909b74245ccff0810713f030316fd
neerajgrg <neerajgrg93@gmail.com>
2022-11-01 23:19:37 +0530

    Create component3.js

---
+++ blocks/component3.js
@@ -0,0 +1,9 @@
+ var body = DocumentApp.getActiveDocument().getBody();
+      // Create a two-dimensional array containing the cell contents.
+  var cells = [
+    ['Component3 Row 1, Cell 1', 'Row 1, Cell 2'],
+    ['Row 2, Cell 1', 'Row 2, Cell 2']
+  ];
+
+  // Build a table from the array.
+  body.appendTable(cells);
