e341736b1ed8b789032c160dca06d96d7c267a64
neerajgrg <neerajgrg93@gmail.com>
2022-11-01 23:18:46 +0530

    Update and rename component.js to component1.js

--- blocks/component1.js
+++ blocks/component1.js
@@ -1,7 +1,7 @@
   var body = DocumentApp.getActiveDocument().getBody();
       // Create a two-dimensional array containing the cell contents.
   var cells = [
-    ['Row 1, Cell 1', 'Row 1, Cell 2'],
+    ['Component1 Row 1, Cell 1', 'Row 1, Cell 2'],
     ['Row 2, Cell 1', 'Row 2, Cell 2']
   ];
 
