da74b47bf4eab64e45360048e81d5654870458ed
Neeraj <neeraj@neerajtekimbp.corp.adobe.com>
2022-11-02 10:16:28 +0530

    updated

--- blocks/cards/doc-element.js
+++ blocks/cards/doc-element.js
@@ -1,7 +1,7 @@
   var body = DocumentApp.getActiveDocument().getBody();
       // Create a two-dimensional array containing the cell contents.
   var cells = [
-    ['Component1 Row 1, Cell 1', 'Row 1, Cell 2'],
+    ['CARD COMPONENT'],
     ['Row 2, Cell 1', 'Row 2, Cell 2']
   ];
 
