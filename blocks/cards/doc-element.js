7b282020efa1565ad145d720207f8d8b81bbb292
neerajgrg <neerajgrg93@gmail.com>
2022-11-02 10:39:41 +0530

    Update doc-element.js

--- blocks/cards/doc-element.js
+++ blocks/cards/doc-element.js
@@ -1,10 +1,12 @@
-  var body = DocumentApp.getActiveDocument().getBody();
-      // Create a two-dimensional array containing the cell contents.
-  var cells = [
-    ['CARD COMPONENT',''],
-    ['Row 2, Cell 1', 'Row 2, Cell 2']
-  ];
 
-  var table = body.appendTable(cells);
-   
+
+var body = DocumentApp.getActiveDocument().getBody();
+      // Create a two-dimensional array containing the cell contents.
+var cells = [
+  ['  CARDS '],
+  ['Row 2, Cell 1', 'Row 2, Cell 2']
+];
+
+// Build a table from the array.
+body.appendTable(cells);
 
