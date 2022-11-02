aa97f2a3ea0e3eb3abd2b8ebea5a2848fb5413f1
neerajgrg <neerajgrg93@gmail.com>
2022-11-02 23:04:55 +0530

    Update doc-element.js

--- blocks/cards/doc-element.js
+++ blocks/cards/doc-element.js
@@ -3,8 +3,8 @@
 var body = DocumentApp.getActiveDocument().getBody();
       // Create a two-dimensional array containing the cell contents.
 var cells = [
-  ['  CARDS ',''],
-  ['Row 2, Cell 1', 'Row 2, Cell 2']
+  ['  Cards '],
+  [' <card_image> ', ' <card_description> ']
 ];
 
 // Build a table from the array.
