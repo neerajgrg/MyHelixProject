acb2b028dcee08efbf0739ed35f417b8da5c83ac
neerajgrg <neerajgrg93@gmail.com>
2022-12-08 23:44:35 +0530

    Update doc-element.js

--- blocks/cards/doc-element.js
+++ blocks/cards/doc-element.js
@@ -1,6 +1,6 @@
 
 [
   ['  Cards '],
-  [' https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png ', ' <card_description> ']
+  ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' <card_description> ']
 ];
 
