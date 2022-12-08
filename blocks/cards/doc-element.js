64023d1e98c34ab1b568ff388852bbc5af6deebd
neerajgrg <neerajgrg93@gmail.com>
2022-12-08 17:18:49 +0530

    Update doc-element.js

--- blocks/cards/doc-element.js
+++ blocks/cards/doc-element.js
@@ -1,22 +1,6 @@
 
-
-var doc = DocumentApp.getActiveDocument();
-var body = doc.getBody();
-
-      // Create a two-dimensional array containing the cell contents.
-var table = [
+[
   ['  Cards '],
   [' <card_image> ', ' <card_description> ']
 ];
-
-// Build a table from the array.
-var cursor = doc.getCursor();
-var element = cursor.getElement();
-var parent = element.getParent();
-//here table is an array of cells like
-// [[r1c1, r1c2], [r2c1, r2c2]]
-body.insertTable(parent.getChildIndex(element) + 1, table);
-
-
-//body.appendTable(cells);
 
