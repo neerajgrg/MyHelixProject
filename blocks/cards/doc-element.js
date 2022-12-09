2e4c05630e2aaf3a7e0888c26c8852a1ab3360ed
Neeraj <neeraj@NeerajtekiMacBook-Pro.local>
2022-12-09 11:16:37 +0530

    block doc elment updated

--- blocks/cards/doc-element.js
+++ blocks/cards/doc-element.js
@@ -3,8 +3,6 @@
   ['  Cards '],
   ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' Unmatched speedHelix is the fastest way to publish, create, and serve websites'],
   ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' Unmatched speedHelix is the fastest way to publish, create, and serve websites'],
-  ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' Helix allows you to publish more content in shorter time with smaller teams  '],
-  ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' Unmatched speedHelix is the fastest way to publish, create, and serve websites'],
-  ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' Unmatched speedHelix is the fastest way to publish, create, and serve websites']
+  ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' Helix allows you to publish more content in shorter time with smaller teams  ']
 ];
 
