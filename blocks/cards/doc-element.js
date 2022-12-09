dc9a10db514046eb10a4e222977deb3a07d66021
neerajgrg <neerajgrg93@gmail.com>
2022-12-09 11:06:33 +0530

    Update doc-element.js

--- blocks/cards/doc-element.js
+++ blocks/cards/doc-element.js
@@ -1,7 +1,6 @@
 
 [
   ['  Cards '],
-  ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' Unmatched speed Helix is the fastest way to publish, create, and serve websites
- ']
+  ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/cards/preview.png', ' card description ']
 ];
 
